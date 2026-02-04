007ef475835541a31b44dc9562b25fd9561e0471
LakshanaR-07 <rajkumarlavanya2003_bit27@mepcoeng.ac.in>
2026-02-04 10:51:03 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j=0,n;
+        n=nums.length;
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] { i, j };
+            }}
+        }
+        return new int[0];}}
